682c7527feada651d3111906de0a2ccd498cf9a8
coldlombax <coldlombax@gmail.com>
2021-06-18 15:28:09 +0100

    Display template with state value.

--- src/components/Calculator/Display.jsx
+++ src/components/Calculator/Display.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Display() {
+function Display({ theme }) {
+  const [displayValue] = useState('320,444');
   return (
-    <div />
+    <div className={` bg-${theme}-screen-bg w-full rounded-xl flex justify-end items-center`}>
+      <p className="mr-5 text-3xl pt-7 pb-7">{displayValue}</p>
+    </div>
   );
 }
 
 Display.propTypes = {
-
+  theme: PropTypes.string.isRequired,
 };
 
 export default Display;
