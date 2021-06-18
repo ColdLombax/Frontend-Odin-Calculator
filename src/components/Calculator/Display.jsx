51ec348036cc75ab4ea5698d33733ceb6f1d50a0
coldlombax <coldlombax@gmail.com>
2021-06-18 14:13:30 +0100

    Component template ready for use.

---
+++ src/components/Calculator/Display.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Display() {
+  return (
+    <div />
+  );
+}
+
+Display.propTypes = {
+
+};
+
+export default Display;
