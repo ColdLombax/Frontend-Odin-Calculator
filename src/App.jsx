b915b1d02d335711aa6cdfd964deb338318a6a8f
coldlombax <coldlombax@gmail.com>
2021-06-18 14:12:18 +0100

    Header, Calculator component added, themes now update on toggle change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Calculator from './components/Calculator';
+import Header from './components/Header';
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState('dark');
+
+  useEffect(() => {}, [currentTheme]);
+
   return (
-    <div className="App" />
+    <div className={`w-full h-screen bg-${currentTheme}-primary text-${currentTheme}-font flex flex-col items-center pt-10`}>
+      <Header changeTheme={setCurrentTheme} theme={currentTheme} />
+      <Calculator theme={currentTheme} />
+    </div>
   );
 }
 
