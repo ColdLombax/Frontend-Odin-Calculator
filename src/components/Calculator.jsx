6bc16f73160ac3865fb8ae57e830f1d3c7bec368
coldlombax <coldlombax@gmail.com>
2021-06-18 15:27:41 +0100

    Now displays Input & Screen components.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Display from './Calculator/Display';
+import Input from './Calculator/Input';
+
 function Calculator({ theme }) {
   return (
-    <div className={`${theme}`} />
+    <div className="flex flex-col w-3/4 mt-10">
+      <Display theme={theme} />
+      <Input theme={theme} />
+    </div>
   );
 }
 
